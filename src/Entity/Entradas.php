d12a3cd02d2d045785440aba1598fa73678ba8be
jualga02 <juan.ramon.alfa@gmail.com>
2025-05-03 07:48:56 +0200

    Primer commit de mi API de Symfony. Hasta aquí todo ha sido automático. A partir de ahora empieza el desarrollo real.

---
+++ src/Entity/Entradas.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EntradasRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EntradasRepository::class)]
+class Entradas
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $email = null;
+
+    #[ORM\Column]
+    private ?float $precio = null;
+
+    #[ORM\Column]
+    private ?bool $ocupado = null;
+
+    #[ORM\Column]
+    private ?int $numeroDeAsiento = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPrecio(): ?float
+    {
+        return $this->precio;
+    }
+
+    public function setPrecio(float $precio): static
+    {
+        $this->precio = $precio;
+
+        return $this;
+    }
+
+    public function isOcupado(): ?bool
+    {
+        return $this->ocupado;
+    }
+
+    public function setOcupado(bool $ocupado): static
+    {
+        $this->ocupado = $ocupado;
+
+        return $this;
+    }
+
+    public function getNumeroDeAsiento(): ?int
+    {
+        return $this->numeroDeAsiento;
+    }
+
+    public function setNumeroDeAsiento(int $numeroDeAsiento): static
+    {
+        $this->numeroDeAsiento = $numeroDeAsiento;
+
+        return $this;
+    }
+}
