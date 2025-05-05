f41fd56f8dfd9c7364fd45f00d125b2432cd9ab5
jualga02 <juan.ramon.alfa@gmail.com>
2025-05-05 12:47:44 +0200

    Primer commit

--- migrations/Version20250505093815.php
+++ migrations/Version20250505093815.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250503052820 extends AbstractMigration
+final class Version20250505093815 extends AbstractMigration
 {
     public function getDescription(): string
     {
--- src/Controller/EntradasController.php
+++ src/Controller/EntradasController.php
@@ -4,16 +4,79 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\EntradasRepository;
+use App\Entity\Entradas;
 
+#[Route('/entradas', name: 'app_entradas')]
 final class EntradasController extends AbstractController
 {
-    #[Route('/entradas', name: 'app_entradas')]
+
+    private EntradasRepository $entradasRepository;
+
+    public function __construct(EntradasRepository $entrada){
+        $this->entradasRepository = $entrada; 
+    }
+
+    #[Route('/', name: 'app_main')]
     public function index(): JsonResponse
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/EntradasController.php',
         ]);
+    }
+
+    #[Route('/new', name:'api_entradas_new',methods:['POST'])]
+    public function add(Request $request):JsonResponse
+    {
+        $data = json_decode($request->getContent());
+        $this->entradasRepository->new($data->email, 
+                                       $data->precio, 
+                                       $data->ocupado, 
+                                       $data->numero_de_asiento);
+        return new JsonResponse(['status' => 'Entrada creada'], Response::HTTP_CREATED);
+    }
+
+    #[Route('/{id}', name: 'api_entradas_show', methods:['GET'])]
+    public function show(Entradas $entrada):JsonResponse
+    {
+        $data = [
+            'id' => $entrada->getId(),
+            'email' => $entrada->getEmail(),
+            'precio' => $entrada->getPrecio(),
+            'ocupado' => $entrada->isOcupado(),
+            'numero_de_asiento' => $entrada->getNumeroDeAsiento(),
+        ];
+        return new JsonResponse($data, Response::HTTP_OK);
+    }
+
+    #[Route('/edit/{id}', name: 'app_entradas_edit', methods:['PUT','PATCH'])]
+    public function edit($id, Request $request): JsonResponse
+    {
+        $entrada = $this->entradasRepository->find($id);
+        //Recibimos los datos como si fueran un objeto
+        $data = json_decode($request->getContent());
+        if($_SERVER['REQUEST_METHOD']=='PUT')
+            $mensaje = 'Tarea actualizada completamente de forma satisfactoria';
+        else
+            $mensaje = 'Tarea actualizada parcialmente de forma satisfactoria';
+        //Utilizamos el operador ternario 
+        empty($data->email) ? true : $entrada->setEmail($data->email);
+        empty($data->precio) ? true : $entrada->setPrecio($data->precio);
+        empty($data->ocupado) ? true : $entrada->setOcupado($data->ocupado);
+        empty($data->numeroDeAsiento) ? true : $entrada->setNumeroDeAsiento($data->numeroDeAsiento);
+        $this->entradasRepository->save($entrada,true);
+        return new JsonResponse(['status'=>$mensaje], Response::HTTP_CREATED);
+    }
+
+    #[Route('/delete/{id}', name:"app_entradas_delete", methods:['DELETE'])]
+    public function remove(Entradas $entrada): JsonResponse
+    {
+        $email = $entrada->getEmail();
+        $this->entradasRepository->remove($entrada, true);
+        return new JsonResponse(['status'=>'Tarea ' . $email . ' borrada'], Response::HTTP_OK);
     }
 }
